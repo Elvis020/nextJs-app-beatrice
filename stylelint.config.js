35fe4cd10ffd0a392013614b6682713d88319739
beatrice.abraham <beatrice.abraham@turntabl.io>
2025-06-17 07:59:05 +0000

    updated the eslint and stylelint  file

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    "import-notation": "off",
+    "rule-empty-line-before": "off",
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["theme"]
+      }
+    ]
+  }
+};
